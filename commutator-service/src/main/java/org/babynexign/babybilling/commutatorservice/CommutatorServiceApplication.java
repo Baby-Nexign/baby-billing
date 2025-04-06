e798941b6366ec7eec84be92b62796c21dfc8d0c
KonstantinFrantz <356156@edu.itmo.ru>
2025-04-06 20:43:15 +0300

    feat: add commutator service init files

---
+++ commutator-service/src/main/java/org/babynexign/babybilling/commutatorservice/CommutatorServiceApplication.java
@@ -0,0 +1,13 @@
+package org.babynexign.babybilling.commutatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommutatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommutatorServiceApplication.class, args);
+	}
+
+}
