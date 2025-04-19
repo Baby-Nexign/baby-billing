7791617580cfc09b81048da5c736ff776ed940c3
KonstantinFrantz <356156@edu.itmo.ru>
2025-04-19 20:26:10 +0300

    refactor: change record to call

--- commutator-service/src/main/java/org/babynexign/babybilling/commutatorservice/repository/CallRepository.java
+++ commutator-service/src/main/java/org/babynexign/babybilling/commutatorservice/repository/CallRepository.java
@@ -1,12 +1,12 @@
 package org.babynexign.babybilling.commutatorservice.repository;
 
-import org.babynexign.babybilling.commutatorservice.entity.Record;
+import org.babynexign.babybilling.commutatorservice.entity.Call;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CallRepository extends JpaRepository<Record, Long> {
-    Page<Record> findAllByOrderByCallStartAsc(Pageable pageable);
+public interface CallRepository extends JpaRepository<Call, Long> {
+    Page<Call> findAllByOrderByCallStartAsc(Pageable pageable);
 }
